467b54c601c0d42d0f64bc1675b7e2a36a12a559
Joydev <23f2000298@ds.study.iitm.ac.in>
2026-01-18 22:01:19 +0530

    first commit

---
+++ application/config.py
@@ -0,0 +1,15 @@
+class Config():
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class LocalDevelopmentConfig(Config):
+    # configuration
+    SQLALCHEMY_DATABASE_URI = "slite:///lmsv2.sqlite3"
+    DEBUG = True
+
+    #config for security
+    SECRET_KEY = "this-is-a-secretkey" #hash user creds in session
+    SECURITY_PASSWORD_HASH = "bcrypt"   #mechanism for hashing pssword
+    SECURITY_PASSWORD_SALT = "this-is-s-password-salt"  #it helps in hashing password
+    WTF_CSRF_ENABLED = False
+    SECURITY_TOKEN_AUTHENTICATION_HEADER = "Authentication-Token"
